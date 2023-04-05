b85588d5bb782bae825ed266d1b3396e505d226d
Anna Belyaeva <anya.belyaeva.1998@list.ru>
2023-04-05 15:59:01 +0300

    Parameterized tests

--- src/test/java/ru/netology/services/ScheduleCalcServiceTest.java
+++ src/test/java/ru/netology/services/ScheduleCalcServiceTest.java
@@ -1,31 +1,20 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 
 public class ScheduleCalcServiceTest {
-    @Test
-    public void shouldCalc10_000() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/parameters.csv")
+    public void shouldCalc(int expected, int income, int expenses, int threshold) {
 
         ScheduleCalcService service = new ScheduleCalcService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
     }
-
-    @Test
-    public void shouldCalc100_000() {
-
-        ScheduleCalcService service = new ScheduleCalcService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
 }
